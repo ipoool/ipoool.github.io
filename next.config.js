3c610d07d7e9bc9c6b07cc9238ccaca92747c032
ipoool <asepulloh0109@gmail.com>
2021-11-14 12:24:23 +0700

    remove base url

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 module.exports = {
   reactStrictMode: false,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     domains: ["storage.googleapis.com"],
   },
-  //   exportPathMap: async function () {
-  //     return {
-  //       "/": {
-  //         page: "/",
-  //       },
-  //     };
-  //   },
+  exportPathMap: async function () {
+    return {
+      "/": {
+        page: "/",
+      },
+    };
+  },
 };
